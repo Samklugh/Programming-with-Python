4352f717de43579016e25127ac20654874c508d2
Samson Oladimeji Odunaiya <102425294+Samklugh@users.noreply.github.com>
2026-01-25 22:07:59 +0000

    Delete week 1/boxes.py

--- week 1/boxes.py
+++
@@ -1,7 +0,0 @@
-import math
-
-no_of_items= float(input("Enter the number of items: "))
-item_per_box= float(input("Enter the number of items per box: "))
-boxes_needed= int(math.ceil(no_of_items/item_per_box))
-
-print(f"For {(no_of_items)}, packing {item_per_box} items in each box, you will need {boxes_needed} boxes.")
